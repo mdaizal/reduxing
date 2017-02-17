2596ad258e2633f62518a7baaa8df03ca6e3f366
mdaizal <mohdaizal@gmail.com>
2017-02-17 17:52:27 +0800

    add counter app

--- src/index.js
+++ src/index.js
@@ -1,33 +1,9 @@
 import { createStore } from 'redux'
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
 
-const counter = (state = 0, action) => {
-  switch(action.type) {
-    case 'INCREMENT':
-      return state + 1
-    case 'DECREMENT':
-      return state - 1
-    default:
-      return state
-  }
-}
-
-const store = createStore(counter)
-
-console.log(`Original state: ${store.getState()}`)
-
-store.dispatch({ type: 'INCREMENT' })
-
-console.log(`Updated state after INCREMENT: ${store.getState()}`)
-
-store.dispatch({ type: 'DECREMENT' })
-
-console.log(`Updated state after DECREMENT: ${store.getState()}`)
-
-store.dispatch({ type: 'INCREMENT' })
-store.dispatch({ type: 'INCREMENT' })
-
-console.log(`Updated state after 2x INCREMENT: ${store.getState()}`)
-
-store.dispatch({ type: 'GILA BABI' })
-
-console.log(`State returned to recent state when action type not matching: ${store.getState()}`)
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+)
