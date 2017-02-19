7fbba197b18eb126a227f185c601b56e1b41acb9
Mohd Aizal <Mohd Aizal>
2017-02-20 05:50:56 +0800

    add todo app

--- src/App.js
+++ src/App.js
@@ -1,72 +1,23 @@
 import React, { Component } from 'react'
-import { createStore } from 'redux'
-
-const counter = (state = 0, action) => {
-  switch(action.type) {
-    case 'INCREMENT':
-      return state + 1
-    case 'DECREMENT':
-      return state - 1
-    default:
-      return state
-  }
-}
-
-const store = createStore(counter)
+import CounterButton from './Counter'
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    
-    this.IncrementButton = this.IncrementButton.bind(this)
-    this.DecrementButton = this.DecrementButton.bind(this)
-  }
-
-  IncrementButton(e) {
-    store.dispatch({ type: 'INCREMENT' })
-    document.getElementById('lbl').innerHTML = store.getState()
-  }
-
-  DecrementButton(e) {
-    store.dispatch({ type: 'DECREMENT' })
-    document.getElementById('lbl').innerHTML = store.getState()
-  }
-
-  componentDidMount() {
-    document.getElementById('lbl').innerHTML = store.getState()
-  }
 
   render() {
     return(
       <div>
-        <button type="submit" onClick={ this.IncrementButton } value="INC">+</button>
-        <button type="submit" onClick={ this.DecrementButton } value="DEC">-</button>
-        <br />
-        <br />
-        <label id="lbl"></label>
-        <label id="lbl2"></label>
+        <div className="row">
+          <div className="col-lg-12">
+            <div className="jumbotron">
+              <h1>REDUXING</h1>
+              <p>Everything about Redux</p>
+            </div>
+          </div>
+        </div>
+        <CounterButton />
       </div>
     )
   }
 }
 
 export default App
-
-// console.log(`Original state: ${store.getState()}`)
-
-// store.dispatch({ type: 'INCREMENT' })
-
-// console.log(`Updated state after INCREMENT: ${store.getState()}`)
-
-// store.dispatch({ type: 'DECREMENT' })
-
-// console.log(`Updated state after DECREMENT: ${store.getState()}`)
-
-// store.dispatch({ type: 'INCREMENT' })
-// store.dispatch({ type: 'INCREMENT' })
-
-// console.log(`Updated state after 2x INCREMENT: ${store.getState()}`)
-
-// store.dispatch({ type: 'GILA BABI' })
-
-// console.log(`State returned to recent state when action type not matching: ${store.getState()}`)
